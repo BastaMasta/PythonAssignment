024c7287d052281d2a73f4440c229f69cea3eff2
BastaMasta <bastamasta3150@proton.me>
2023-08-26 03:43:17 +0530

    Added quiz.py added a bunch of todos

---
+++ Quiz/quiz.py
@@ -0,0 +1,13 @@
+with open("Quiz.txt") as f:
+    pass
+# TODO: Read contents
+
+# TODO: Segregate questions and answers into dicts
+
+# TODO: Randomly select questions
+
+# TODO: Display selected questions
+
+# TODO: Take in answers and verify
+
+# TODO: Tally and display scores at the end
